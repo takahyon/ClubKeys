3609b82c842c748fb91a0b2691aecfed4d05b56d
Takamasa Iijima <takahyon@gmail.com>
2019-01-24 17:38:40 +0900

    [fix]change format

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,4 @@
- # coding: utf-8
+# coding: utf-8
 import os
 import sys
 
@@ -13,41 +13,56 @@ import dynamo
 
 
 def lambda_handler(event, context):
-    #slack.post_slack(str(event))
-    #slack.post_slack(str(body["events"]))
+    # slack.post_slack(str(body["events"]))
     list = {}
     try:
-        body = json.loads(event["body"])
-        for events in body["events"]:
-            type = events["message"]["type"]
+        print(1)
+        # slack.post_slack(1)
+        # body = event["body"]
 
-            if type == "text":
+        for events in event["events"]:
+            datatype = events["message"]["type"]
+
+            if datatype == "text":
                 line.post_text(events)
-            elif type == "sticker":
+            elif datatype == "sticker":
                 line.post_sticker(events)
             else:
                 line.post_text(events)
         return {'statusCode': 200, 'body': event}
     except:
-
+        print(2)
+        # slack.post_slack(2)
         try:
             status = event["status"]
-            try:
+
+            if event["comment"] != "":
+                print(3)
+                # slack.post_slack(3)
                 com = event["comment"]
-                dynamo.change_status(status,comment=com)
-                event = dynamo.get_status()
-                slack.post_slack(event)
+                dynamo.change_status(status, comment=com)
+                # event = dynamo.get_status()
+                slack.post_slack("iOS Client incoming!\n" + str(event))
                 print("iOS Client incoming!")
-            except:
+                return {'statusCode': 200, 'body': "iOS Comming with message success"}
+            else:
+                print(4)
+                # slack.post_slack(4)
                 dynamo.change_status(status)
-                event = dynamo.get_status()
-                slack.post_slack(event)
+                # event = dynamo.get_status()
+                slack.post_slack("iOS Client incoming!set Default\n" + str(event))
                 print("iOS Client incoming! Comment set Default")
-            
+                return {'statusCode': 200, 'body': "iOS Comming success"}
+
         except:
+            import traceback
+            traceback.print_exc()
+
+            print(5)
+            # slack.post_slack(5)
             event = dynamo.get_status()
             list = dynamo.get_log()
             print(event)
-            
-        status = dynamo.get_status_raw()
-        return {'statusCode': 200, 'body': event, 'status':status, 'log':list}
+
+            status = dynamo.get_status_raw()
+            return {'statusCode': 200, 'body': event, 'status': status, 'log': list}
